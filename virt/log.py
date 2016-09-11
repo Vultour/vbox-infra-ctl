f231fde9cbdb886854acde5f3b1c4a1bdfabc4fc
Vultour <vultour@users.noreply.github.com>
2016-09-11 17:30:20 +0100

    Update logger to split stdout/stderr

--- virt/log.py
+++ virt/log.py
@@ -1,7 +1,13 @@
 import logging
+import sys
 
 import config
 
+
+class STDOutFilter(logging.Filter): # Only log WARN and below messages to stdout
+    def filter(self, record):
+        if (record.levelno >= logging.ERROR): return 0
+        return 1
 
 def init_logger(log_level):
     l = logging.getLogger(config.get_config()["app-name"])
@@ -9,10 +15,16 @@ def init_logger(log_level):
 
     formatter = logging.Formatter("%(asctime)s [%(levelname)-5s]: %(message)s")
 
-    ch = logging.StreamHandler()
+    ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(log_level)
     ch.setFormatter(formatter)
+    ch.addFilter(STDOutFilter())
     l.addHandler(ch)
+
+    ch_err = logging.StreamHandler(sys.stderr) # Log ERROR and above messages to stderr
+    ch_err.setLevel(logging.ERROR)
+    ch_err.setFormatter(formatter)
+    l.addHandler(ch_err)
 
     if (config.get_config()["log-file-enabled"]):
         fh = logging.FileHandler(config.get_config()["log-file-path"])
